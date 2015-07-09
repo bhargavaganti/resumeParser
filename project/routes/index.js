5ce8cf5e14431fa67f8a7cf68a6ac3878a0c31de
rkallepu <rashmika23@gmail.com>
2015-07-09 16:59:34 -0700

    extracting email and phone number form the parsed text file

--- project/routes/index.js
+++ project/routes/index.js
@@ -3,7 +3,10 @@ var express = require('express'),
     multer = require('multer'),
     spawn = require('child_process').spawn,
     fs = require('fs'),
-    filename = '';
+    filename = '',
+    email = '',
+    filesObj = {},
+    phone = 0;
 
 /* GET home page.*/
 router.get('/', function(req, res, next) {
@@ -13,18 +16,27 @@ router.use(multer({
   dest:'./upload/'
 }));
 router.post('/', function (req, res){
-  console.log('req.files',req.files);
-  var filename = req.files['file 0'].originalname.split('.')[0];
-  pdftotext    = spawn('pdftotext.exe', ['C:\\Users\\Rashmika\\Documents\\GitHub\\resumeParser\\project\\upload\\' + req.files['file 0'].name, 'C:\\Users\\Rashmika\\Documents\\GitHub\\resumeParser\\project\\text\\' + filename + '.txt'],  {cwd: 'C:\\Users\\Rashmika\\Documents\\xpdf'});
-  pdftotext.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-
-  pdftotext.on('close', function (code) {
-    console.log('child process exited with code ' + code);
-    readContent(filename, function (err, content) {
-     console.log(content);
-     });
+  //console.log('req.files',req.files);
+  filesObj = req.files;
+  //console.log(Object.keys(filesObj));
+  Object.keys(filesObj).forEach(function(index){
+    //console.log(index);
+    var filename = req.files[index].originalname.split('.')[0];
+    pdftotext    = spawn('pdftotext.exe', ['C:\\Users\\Rashmika\\Documents\\GitHub\\resumeParser\\project\\upload\\' + req.files[index].name, 'C:\\Users\\Rashmika\\Documents\\GitHub\\resumeParser\\project\\text\\' + filename + '.txt'],  {cwd: 'C:\\Users\\Rashmika\\Documents\\xpdf'});
+    pdftotext.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+    pdftotext.on('close', function (code) {
+      console.log('child process exited with code ' + code);
+      readContent(filename, function (err, content) {
+        console.log(content);
+        email = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi.exec(content);
+        phone = /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/.exec(content);
+        console.log('email found in the resume',email[0]);
+        console.log('phone number found in the resume',phone[0]);
+        //email = content.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+      });
+    });
   });
   res.status(200).json(req.files);
 });
